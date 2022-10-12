a93d13752dde003329f6557cfe2084c251db16f8
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-10-12 23:21:17 +0600

    A weather app 004 Weather API Headers

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getWeatherData(cityName: string) {
+    this.http.get(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue)
+    });
+  }
 }
+
